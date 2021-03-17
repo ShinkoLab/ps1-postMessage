ff0b4aaaab9fb9d7e772cfb6092d731c354ed746
Shinko Sasaki <dev@shinkolab.jp>
2021-03-17 20:08:38 +0900

    add main script

---
+++ postSlack.ps1
@@ -0,0 +1,28 @@
+﻿# Please Change Values for Your Slack WorkSpace.
+$token = "SLACK_TOKEN"
+$channel = "CHANNEL_NAME"
+$username = "BOT_USERNAME"
+
+Add-Type -AssemblyName System.Web
+
+#$channel = [System.Web.HttpUtility]::UrlEncode($channel)
+$utf8Bytes = [System.Text.Encoding]::UTF8.GetBytes($args)
+$text = [System.Text.Encoding]::GetEncoding('UTF-8').GetString($utf8Bytes);
+#$text = [System.Web.HttpUtility]::UrlEncode($text)
+
+$headerargs = @{}
+$headerargs.Add("Authorization", "Bearer " + $token)
+$headerargs.Add("Content-Type", "application/json;charset=utf-8")
+
+$postargs = [ordered]@{}
+#$postargs.Add("token", $token)
+$postargs.Add("channel", $channel)
+$postargs.Add("username", $username)
+$postargs.Add("text", $text)
+
+$postJson = ConvertTo-Json -Compress $postargs
+
+echo $postargs
+echo $postJson
+#Invoke-RestMethod -Uri "https://slack.com/api/chat.postMessage?token=$token&channel=$channel&text=$text&username=$username" -Method Get
+Invoke-RestMethod -Method Post -Uri https://slack.com/api/chat.postMessage -Headers $headerargs -Body $postJson
